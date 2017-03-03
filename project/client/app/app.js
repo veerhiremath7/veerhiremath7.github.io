c357605c3f34e30d58b9cf30ed36c59536885b89
veeresh <veerhiremath7@gmail.com>
2017-03-03 21:19:43 +0530

    removed chat project from repo and added resume download option

--- project/client/app/app.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular
-  .module('app', ['pubnub.angular.service', 'ngNotify', 'satellizer', 'ngSanitize', 'ngToast', 'ui.router', 'angular-loading-bar'])
--- project/client/app/components/friends-search-box/friends-search-box.directive.js
+++
@@ -1,8 +0,0 @@
-angular.module('app').directive('friendsSearchBox', function() {
-  return {
-    restrict: "E",
-    replace: true,
-    templateUrl: 'components/friends-search-box/friends-search-box.html',
-    scope: false
-  };
-});
